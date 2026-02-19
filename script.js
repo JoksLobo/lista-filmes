a0c8bc90d936b00a620432a9a955d4455b6cd49b
JoksLobo <jocson_herry@hotmail.com>
2026-02-19 13:11:58 -0300

    integração da api

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ const overlay = document.getElementById("modal-overlay");
 const searchName = document.getElementById("nome");
 const searchYear = document.getElementById("ano");
 const key = "66508f0";
-function searchButtonClickHandler() {
+async function searchButtonClickHandler() {
   overlay.classList.add("open");
 
   const name = searchName.value.split(" ").join("+");
   const year = searchYear.value;
+  let url = `http://www.omdbapi.com/?apikey=${key}&t=${name}&y=${year}`;
+
+  const responde = await fetch(url);
+  const data = await responde.json();
+  console.log(data);
 }
 
 searchButton.addEventListener("click", searchButtonClickHandler);
