d0e017d769728236cc59053e4d45025e8a3996a9
JoksLobo <jocson_herry@hotmail.com>
2026-02-19 12:42:24 -0300

    capturando os campos de pesquisa

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const searchButton = document.getElementById("search-btn");
 const overlay = document.getElementById("modal-overlay");
+const searchName = document.getElementById("nome");
+const searchYear = document.getElementById("ano");
+const key = "66508f0";
 function searchButtonClickHandler() {
   overlay.classList.add("open");
+
+  const name = searchName.value.split(" ").join("+");
+  const year = searchYear.value;
 }
 
 searchButton.addEventListener("click", searchButtonClickHandler);
